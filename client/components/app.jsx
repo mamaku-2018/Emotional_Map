f4c8c7e8cd6768a7fd9778e6e625a1ad718328de
MichaelMitch <michael.j.mitchelson@gmail.com>
2018-07-19 10:38:51 +1200

    about to pull from master

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import {Route} from 'react-router-dom'
-
+import Map from './Map'
 const App = () => {
   return (
     <div className='container'>
       <h1> hello </h1>
+      <Map/>
     </div>
   )
 }
