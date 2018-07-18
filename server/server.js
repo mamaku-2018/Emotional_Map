84ed3a06bb4cb087f226140ae1643be32e03e549
sungjin-shane <sjpark731015@gmail.com>
2018-07-19 10:24:08 +1200

    first commit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const path = require('path')
+const express = require('express')
+// const companyRoutes = require('./routes/companyRoutes')
+// const statsRoutes = require('./routes/statsRoutes')
+// const authRoutes = require('./routes/authRoutes')
+
+const server = express()
+
+server.use(express.json())
+server.use(express.static(path.join(__dirname, 'public')))
+
+// server.use('/api/v1/companies', companyRoutes)
+// server.use('/api/v1/stats', statsRoutes)
+// server.use('/api/v1/auth', authRoutes)
+
+module.exports = server
