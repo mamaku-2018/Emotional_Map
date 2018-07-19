d46cbd4f4d7c0f897a3e3fbbe31f8c1cdb85df67
mattjackmcgregor <mattjackmcgregor@gmail.com>
2018-07-19 15:06:38 +1200

    was pluged in forgot to commit. working on the markers and seting up the shape of the pages to pass things through/components

--- client/components/ViewMap.jsx
+++ client/components/ViewMap.jsx
@@ -2,15 +2,20 @@ import React from 'react'
 import connect from 'react-redux'
 import L from 'leaflet'
 class ViewMap extends React.Component {
-  componentDidMount(){
-    this.map()
+  componentDidMount () {
+    // create map
+    this.map = L.map('map', {
+      center: [-36.8484, 174.7622],
+      zoom: 16,
+      layers: [
+        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+          attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        })
+      ]
+    })
+    this.marker = L.marker(this.props.markerPosition).addTo(this.map)
   }
-  map () {
-    var map  = L.map('map').setView([-36.848460, 174.763332], 12)
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map)
-  }
+
   render () {
     return <div className="Map" id="map">xx</div>
   }
---
+++ client/components/childComponents/Markers.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import connect from 'react-redux'
+import L from 'leaflet'
+class Markers extends React.Component {
+  componentDidMount () {
+    this.marks()
+  }
+  marks () {
+    var marks = L.marker([36.8484, 174.7622])
+  }
+  render () {
+    return (
+      this.
+    )
+    
+
+  }
+}
+
+export default ViewMap
