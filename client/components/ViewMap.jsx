e5e7a30d097a328b1c926dee29c8a0dc4726e2a0
MichaelMitch <michael.j.mitchelson@gmail.com>
2018-07-20 10:09:19 +1200

    map now showing on AK in ViewMap

--- client/components/ViewMap.jsx
+++ client/components/ViewMap.jsx
@@ -1,30 +1,20 @@
 import React from 'react'
-import connect from 'react-redux'
-import getPinInfo from '../actions/mapping'
+import {connect} from 'react-redux'
 import {Map, TileLayer, Marker,Popup} from 'react-leaflet'
 
 class ViewAreas extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-      lat: 51.505,
-      lng: -0.09,
-      zoom: 13
-      }
+    }
   }
   render() {
     const position = [this.state.lat, this.state.lng]
     return (
-      <Map center={position} zoom={this.state.zoom}>
-        <TileLayer
+      <Map className ="Map" center= {[-36.8485,174.7633]} zoom={12}>
+        <TileLayer 
           attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={position}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
       </Map>
     )
   }
