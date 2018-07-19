d8b769fa141a063f6d2552dfee7375d560b6aa68
MichaelMitch <michael.j.mitchelson@gmail.com>
2018-07-20 09:47:09 +1200

    restructured some of the files

--- server/routes/mapRoutes.js
+++ server/routes/mapRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const db = require('../db/mapping')
-router.get('/map', (req, res) => {
+router.get('/getPins', (req, res) => {
     db.getPins()
     .then(pins => {
-      res.json(pins)
+      req.json(pins)
     })
     .catch(err => {
       // eslint-disable-next-line
@@ -12,5 +12,26 @@ router.get('/map', (req, res) => {
       res.status(500).send('Unable to get pins')
     })
 })
+
+router.get('/getAreas', (req,res) => {
+  db.getAreas()
+  .then(areas => {
+    res.json(areas)
+  })
+  .catch(err => {
+    // eslint-disable-next-line
+    console.log(err)
+    res.status(500).send('Unable to get pins')
+  })
+})
+
 module.exports = router
-c
+
+function getDate () {
+  const date = new Date()
+  const dd = date.getDate()
+  const mm = date.getMonth() + 1
+  const yyyy = date.getFullYear()
+  const datestr = `${yyyy}-${mm}-${dd}`
+  return datestr
+}
