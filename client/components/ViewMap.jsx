0b875b64050d888f78f2ec3e1b414d7ebda56eb8
Rachael Peri <rachael.peri@gmail.com>
2018-07-20 10:17:59 +1200

    created viewpins component

--- client/components/ViewMap.jsx
+++ client/components/ViewMap.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import connect from 'react-redux'
 import L from 'leaflet'
 class ViewMap extends React.Component {
-  componentDidMount(){
+  componentDidMount () {
     this.map()
   }
   map () {
-    var map  = L.map('map').setView([-36.848460, 174.763332], 12)
+    var map = L.map('map').setView([-36.848460, 174.763332], 12)
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map)
---
+++ client/components/ViewPins.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Route} from 'react-router-dom'
+
+const ViewPins = () => {
+  return (
+
+    <div className='view-container'>
+      <h1>view pins</h1>
+    </div>
+  )
+}
+
+export default ViewPins
