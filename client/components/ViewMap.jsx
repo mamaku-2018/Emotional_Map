9c9593b51c95a4f35a2f77f3650f290c756a49e0
mattjackmcgregor <mattjackmcgregor@gmail.com>
2018-07-19 16:40:16 +1200

    click event to add pin sucessful

--- client/components/ViewMap.jsx
+++ client/components/ViewMap.jsx
@@ -1,46 +1,45 @@
 import React from 'react'
-import connect from 'react-redux'
-const { Map, TileLayer, Marker } = window.ReactLeaflet
+// import connect from 'react-redux'
+import {Map, TileLayer, Marker} from 'react-leaflet'
+// const {Map, TileLayer, Marker} = window.ReactLeaflet
 
 class ViewMap extends React.Component {
-  constructor() {
+  constructor () {
     super()
     this.state = {
       markers: [[-36.8484, 174.7622]]
     }
+    this.addMarker = this.addMarker.bind(this)
   }
 
-  addMarker = (e) => {
-    const {markers} = this.state
+  addMarker (e) {
+    const markers = this.state.markers
     markers.push(e.latlng)
     this.setState({markers})
   }
 
-  render() {
+  render () {
     return (
-      <Map 
-        center={[-36.8484, 174.7622]} 
-        onClick={this.addMarker}
-        zoom={13} 
+      <div id="Map">
+        <Map id="map"
+          center={[-36.8484, 174.7622]}
+          onClick={this.addMarker}
+          zoom={13}
         >
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
-        />
-        {this.state.markers.map((position, idx) => 
-          <Marker key={`marker-${idx}`} position={position} />
-    
-        
-        )}
-      </Map>
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+          />
+          {this.state.markers.map((position, idx) =>
+            <Marker key={`marker-${idx}`} position={position} />
+
+          )}
+        </Map>
+      </div>
     )
   }
-
+}
 export default ViewMap
-
-
-
-
 
 // componentDidMount () {
 //   // create map
