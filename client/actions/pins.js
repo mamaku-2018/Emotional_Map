3185b3235d3907437539f87aa19d08c539575794
Rachael Peri <rachael.peri@gmail.com>
2018-07-19 15:03:04 +1200

    removed mapping.js actions

--- client/actions/pins.js
+++ client/actions/pins.js
@@ -2,6 +2,9 @@ import request from 'superagent'
 
 import {showError} from './index'
 
+export const REQUEST_PIN_INFO = 'REQUEST_PIN_INFO'
+export const RECEIVE_PIN_INFO = 'RECEIVE_PIN_INFO'
+
 export function addPin (pin) {
   return (dispatch) => {
     return request.post('/api/v1/add')
@@ -25,5 +28,19 @@ export const receivePinInfo = (pinInfo) => {
   return {
     type: RECEIVE_PIN_INFO,
     pinInfo
+  }
+}
+
+export const getpins = () => {
+  return (dispatch) => {
+    dispatch(requestPinInfo())
+    return request
+      .get('api/v1/map')
+      .then(res => {
+        dispatch(receivePinInfo(res.body))
+      })
+      .catch(err => {
+        dispatch(showError(err.message))
+      })
   }
 }
