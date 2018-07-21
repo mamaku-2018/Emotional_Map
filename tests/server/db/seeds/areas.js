7ada64734ce342b7c4b0b854de9988b70c5ca498
Matthew McGregor <Mattjackmcgregor@gmail.com>
2018-07-21 19:47:55 +1200

    mt ede nboundry aprox, updated seed info still working on it

--- tests/server/db/seeds/areas.js
+++ tests/server/db/seeds/areas.js
@@ -1,8 +1,21 @@
 exports.seed = (knex, Promise) => {
   return knex('areas').del()
     .then(() => {
-      return knex('areas').insert([
-        {area_id: 1, area_name: 'city center', area_lat1: -36.848461, area_long1: 174.763336, area_lat2: -36.908461, area_long2: 174.903336, area_lat3: -36.748461, area_long3: 174.883336, area_lat4: -36.848461, area_long4: 174.763336, last_update_date: '19-07-2018'}
-      ])
+      return knex('areas').insert(
+        {area_id: 1,
+          area_name: 'mt eden',
+          positions: [
+            [-36.867847, 174.759383],
+            [-36.867818, 174.768261],
+            [-36.874631, 174.766384],
+            [-36.877980, 174.767106],
+            [-36.900534, 174.759828],
+            [-36.894807, 174.739922],
+            [-36.877137, 174.745627],
+            [-36.874758, 174.737494],
+            [-36.867621, 174.757461]
+          ]
+        },
+      {area_id:  2})
     })
 }
