3835668d6e522c698d76e6c9a527e29ce6c6c5e4
Caitlyn <caitlyn.bayley@gmail.com>
2018-07-22 14:30:11 +1200

    my update as of 2.30pm

---
+++ client/components/ViewPinsModal.jsx
@@ -0,0 +1,32 @@
+// import React from './react'
+
+// class ViewPinsModal extends React.component
+
+//   return (
+
+
+
+//     {/* <!-- Modal --> */}
+//     <div className="viewModal" id="viewModal" tabIndex="-1" role="dialog">
+//       <div className="modal-dialog" role="document">
+//         <div className="view-modal-content">
+//           <div className="view-modal-header">
+//             <h5 className="view-modal-title" id="ViewModalLabel">View Pins</h5>
+//             <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+//               <span aria-hidden="true">&times;</span>
+//             </button>
+//           </div>
+//           <div className="view-modal-body">
+//             <p>Instructions on how to view pins </p>
+//           </div>
+//           <div className="modal-footer">
+//             <button type="button" className="btn btn-secondary" data-dismiss="modal">Okay, got it!</button>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+
+//   )
+// }
+
+// export default ViewPinsModal
