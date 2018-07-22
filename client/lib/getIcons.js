a1d59457a5124abb66acad4d1056d0940e46873a
Rachael Peri <rachael.peri@gmail.com>
2018-07-22 16:10:27 +1200

    working onchange for emotions with updated icons

--- client/lib/getIcons.js
+++ client/lib/getIcons.js
@@ -9,25 +9,25 @@ export function getIcons (id) {
   })
   switch (id) {
     case 1:
-      myIcon.options.iconUrl = '/images/Dark.png'
+      myIcon.options.iconUrl = '/images/Happy.png'
       break
     case 2:
-      myIcon.options.iconUrl = '/images/Indigo.png'
+      myIcon.options.iconUrl = '/images/Sad.png'
       break
     case 3:
-      myIcon.options.iconUrl = '/images/Pink.png'
+      myIcon.options.iconUrl = '/images/Mad.png'
       break
     case 4:
-      myIcon.options.iconUrl = '/images/Purple.png'
+      myIcon.options.iconUrl = '/images/Scared.png'
       break
     case 5:
-      myIcon.options.iconUrl = '/images/Red.png'
+      myIcon.options.iconUrl = '/images/Powerful.png'
       break
     case 6:
-      myIcon.options.iconUrl = '/images/Yellow.png'
+      myIcon.options.iconUrl = '/images/Peaceful.png'
       break
     default:
-      myIcon.options.iconUrl = '/images/Dark.png'
+      myIcon.options.iconUrl = '/images/Happy.png'
   }
   return myIcon
 }
