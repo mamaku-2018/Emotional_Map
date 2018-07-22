c86389ba32966f1a51088b183d668b726f6dd89b
Rachael Peri <raheraperi@Rachaels-MacBook-Air.local>
2018-07-23 11:09:54 +1200

    changes to view page css

--- client/components/ViewPinInfo.jsx
+++ client/components/ViewPinInfo.jsx
@@ -6,7 +6,7 @@ class ViewPinInfo extends React.Component {
   render () {
     return (
       <div className='ViewPinInfo'>
-        <h3>Pin info</h3>
+        <h3>Pin Story</h3>
         <p>{this.props.onePinInfo.pin_name}</p>
         <p>{this.props.onePinInfo.last_updated_date}</p>
         <p>{this.props.onePinInfo.emotion_type}</p>
