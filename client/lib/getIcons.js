c38cb6e260b28ba2a41103d5524e50cf24d2f80d
Rachael Peri <rachael.peri@gmail.com>
2018-07-22 13:26:54 +1200

    pins reflecting based on emotion type

--- client/lib/getIcons.js
+++ client/lib/getIcons.js
@@ -1,12 +1,31 @@
 import L from 'leaflet'
 
-export function getIcons () {
+export function getIcons (id) {
   const myIcon = L.icon({
     iconUrl: '',
     iconSize: [30, 30],
     iconAnchor: [22, 94],
     popupAnchor: [-3, -76]
   })
-  myIcon.options.iconUrl = '/images/cool.png'
+  switch (id) {
+    case 1:
+      myIcon.options.iconUrl = '/images/Dark.png'
+      break
+    case 2:
+      myIcon.options.iconUrl = '/images/Indigo.png'
+      break
+    case 3:
+      myIcon.options.iconUrl = '/images/Pink.png'
+      break
+    case 4:
+      myIcon.options.iconUrl = '/images/Purple.png'
+      break
+    case 5:
+      myIcon.options.iconUrl = '/images/Red.png'
+      break
+    case 6:
+      myIcon.options.iconUrl = '/images/Yellow.png'
+      break
+  }
   return myIcon
 }
