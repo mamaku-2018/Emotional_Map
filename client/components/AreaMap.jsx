a9e908cd201bcb2fc008cccd38dc69b3aaebe255
mattjackmcgregor <mattjackmcgregor@gmail.com>
2018-07-23 10:15:46 +1200

    importing actions to component

--- client/components/AreaMap.jsx
+++ client/components/AreaMap.jsx
@@ -1,23 +1,14 @@
 import React from 'react'
+import {connect} from 'react-redux'
+
 import {Polygon, Map, TileLayer} from 'react-leaflet'
+import {getAreas} from '../actions/areas'
+
 
 class AreaMap extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      area_id: 2,
-      area_name: 'City Center',
-      positions: [
-        [-36.860136, 174.762389],
-        [-36.847523, 174.782921],
-        [-36.840277, 174.782792],
-        [-36.835856, 174.758227],
-        [-36.843996, 174.750073],
-        [-36.857595, 174.755265],
-        [-36.859277, 174.752648],
-        [-36.861887, 174.754965],
-        [-36.859312, 174.759257]
-      ]
 
     }
   }
