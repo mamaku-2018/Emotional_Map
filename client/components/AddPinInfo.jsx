a4c61ff04099e9123ad949f6fa91dcf7f2dd13db
sungjin-shane <sjpark731015@gmail.com>
2018-07-24 09:47:40 +1200

    clean all

--- client/components/AddPinInfo.jsx
+++ client/components/AddPinInfo.jsx
@@ -78,7 +78,7 @@ export class AddPinInfo extends React.Component {
 
 const mapStateToProps = (state) => {
   return {
-    area: (state.areasInfo),
+    area: state.areasInfo,
     pinPosition: state.pinPosition
   }
 }
