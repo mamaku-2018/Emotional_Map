4033231dc6c2a40df9a81e341a32108630cd52e6
Michael Mitchelson <michael.j.mitchelson@gmail.com>
2018-07-24 10:45:11 +1200

    changed to classbased

--- client/components/HidePinInfo.jsx
+++ client/components/HidePinInfo.jsx
@@ -2,15 +2,20 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {removeOnePin} from '../actions/pins'
 
-const HidePinInfo = (props) => {
-  function hidePin () {
+class HidePinInfo extends React.Component {
+  constructor (props) {
+    super(props)
+    this.hidePin = this.hidePin.bind(this)
+  }
+  hidePin () {
     this.props.dispatch(removeOnePin())
   }
-  return (
-    <div>
-      <img src="/images/DownArrow.png" alt="Hide Pin" onClick={hidePin}/>
-    </div>
-  )
+  render () {
+    return (
+      <div>
+        <img src="/images/DownArrow.png" alt="Hide Pin" onClick={hidePin}/>
+      </div>
+    )
+  }
 }
-
 export default connect()(HidePinInfo)
