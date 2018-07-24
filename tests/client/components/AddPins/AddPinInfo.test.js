7a072c839ae006feb27a24c5d8f9ced60f54fb8b
Michael Mitchelson <michael.j.mitchelson@gmail.com>
2018-07-24 16:46:10 +1200

    created tests

--- tests/client/components/AddPins/AddPinInfo.test.js
+++ tests/client/components/AddPins/AddPinInfo.test.js
@@ -2,13 +2,13 @@ import React from 'react'
 import {configure, shallow} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
-import {AddPinInfo} from '../../../client/components/AddPinInfo'
+import {AddPinInfo} from '../../../../client/components/AddPinInfo'
 configure({adapter: new Adapter()})
 
+const mockFn = jest.fn()
 test('<AddPinInfo />', () => {
-  const expected = 'Add Pin'
-
-  const wrapper = shallow(<AddPinInfo />)
+  const expected = 'share your street feel'
+  const wrapper = shallow(<AddPinInfo dispatch = {mockFn}/>)
   const actual = wrapper.find('h3').text()
 
   expect(actual).toEqual(expected)
