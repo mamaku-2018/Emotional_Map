3f8695d93b85608fc32a980a2e4bdda46f2bde38
sungjin-shane <sjpark731015@gmail.com>
2018-07-24 15:33:55 +1200

    almosto finished filter emotion

--- client/components/ViewPinMap.jsx
+++ client/components/ViewPinMap.jsx
@@ -40,7 +40,8 @@ class ViewPinMap extends React.Component {
 }
 const mapStateToProps = (state) => {
   if (state.pinInfo.length > 0) {
-    return {pinInfo: state.pinInfo
+    return {
+      pinInfo: state.pinInfo
     }
   } else {
     return {pinInfo: []}
--- client/components/ViewPins.jsx
+++ client/components/ViewPins.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import ViewPinMap from './ViewPinMap'
+import ViewFilterEmotion from './ViewFilterEmotion'
 import ViewPinInfo from './ViewPinInfo'
 import {removeOnePin} from '../actions/pins'
+
 class ViewPins extends React.Component {
   render () {
     return (
       <div>
+        <ViewFilterEmotion/>
         <ViewPinMap/>
         {typeof this.props.onePinInfo.pin_name === 'string' && <ViewPinInfo/>}
       </div>
     )
   }
-  componentWillUnmount() {
+  componentWillUnmount () {
     this.props.dispatch(removeOnePin())
   }
 }
