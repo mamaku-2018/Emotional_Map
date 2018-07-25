6c3253917aac8fcd05896ac2854c5bcf0c6e9656
Caitlyn <caitlyn.bayley@gmail.com>
2018-07-25 12:11:32 +1200

    made consistant styling with view info component'

--- client/components/ViewPinInfo.jsx
+++ client/components/ViewPinInfo.jsx
@@ -9,10 +9,9 @@ class ViewPinInfo extends React.Component {
         <h3>{this.props.onePinInfo.pin_name}</h3>
         {/* <p>{this.props.onePinInfo.pin_name}</p> */}
         <p>{this.props.onePinInfo.last_updated_date}</p>
-        <img src={emotInfo.path} alt={emotInfo.name}/>
-        <p>{emotInfo.name}</p>
+        <img className='viewEmo' src={emotInfo.path} alt={emotInfo.name}/>
         {/* <p>{this.props.onePinInfo.emotion_type}</p> */}
-        <p>{this.props.onePinInfo.comment}</p>
+        <p className='commentsView'>{this.props.onePinInfo.comment}</p>
         <div className='PinInfo'>
         </div>
       </div>
